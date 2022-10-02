6d996ca5f9897e3558800f944f1650a309be1ce5
Djulia <djulia07041988@gmail.com>
2022-10-02 13:30:20 +0300

    Мінімальна обробка ситуації виграшу і програшу.

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import my_game
 
 if __name__ == '__main__':
     game = my_game.Game("level/level1")
-    game.run()
+    try:
+        game.run()
+    except my_game.LevelDoneException:
+        print("Level Done!")
+    except my_game.GameOverException:
+        print("Game Over!")
